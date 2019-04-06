48752c6068e84f84ee261e63121cc3f70edead86
Michael <mik@use.startmail.com>
2019-04-07 08:41:46 +1000

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Michael Schembri on 7/4/19.
+//  Copyright © 2019 Michael Schembri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
