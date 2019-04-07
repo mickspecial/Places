2e354caabd78dea6746b03cc250630b1a576261c
Michael <mik@use.startmail.com>
2019-04-07 10:54:34 +1000

    highlight search text

--- Places/FindTableViewController.swift
+++ Places/FindTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import MapKit
 
+// swiftlint:disable line_length
+
 class FindTableViewController: UITableViewController {
 
 	lazy private var searchController: UISearchController = {
@@ -49,8 +51,8 @@ class FindTableViewController: UITableViewController {
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let searchResult = searchResults[indexPath.row]
 		let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-		cell.textLabel?.text = searchResult.title
-		cell.detailTextLabel?.text = searchResult.subtitle
+		cell.textLabel?.attributedText = highlightedText(searchResult.title, inRanges: searchResult.titleHighlightRanges, size: 17.0)
+		cell.detailTextLabel?.attributedText = highlightedText(searchResult.subtitle, inRanges: searchResult.subtitleHighlightRanges, size: 12.0)
 		return cell
 	}
 
@@ -68,6 +70,25 @@ class FindTableViewController: UITableViewController {
 			let coordinate = item.placemark.coordinate
 			print(String(describing: coordinate))
 		}
+	}
+
+	/**
+	Highlights the matching search strings with the results
+	- parameter text: The text to highlight
+	- parameter ranges: The ranges where the text should be highlighted
+	- parameter size: The size the text should be set at
+	- returns: A highlighted attributed string with the ranges highlighted
+	*/
+	func highlightedText(_ text: String, inRanges ranges: [NSValue], size: CGFloat) -> NSAttributedString {
+		let attributedText = NSMutableAttributedString(string: text)
+		let regular = UIFont.systemFont(ofSize: size)
+		let range = NSRange(location: 0, length: text.count)
+		attributedText.addAttribute(NSAttributedString.Key.font, value: regular, range: range)
+		let bold = UIFont.boldSystemFont(ofSize: size)
+		for value in ranges {
+			attributedText.addAttribute(NSAttributedString.Key.font, value: bold, range: value.rangeValue)
+		}
+		return attributedText
 	}
 }
 
