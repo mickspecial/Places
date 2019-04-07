fced3943b7456cb0485ace2277206cf6a3318c21
Michael <mik@use.startmail.com>
2019-04-07 10:47:31 +1000

    search for locations

--- Places/FindTableViewController.swift
+++ Places/FindTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class FindTableViewController: UITableViewController {
 
@@ -15,16 +16,21 @@ class FindTableViewController: UITableViewController {
 		sctrl = UISearchController(searchResultsController: nil)
 		sctrl.obscuresBackgroundDuringPresentation = false
 		sctrl.searchBar.placeholder = "Find Place"
-		sctrl.searchResultsUpdater = self
 		navigationItem.searchController = sctrl
 		navigationItem.hidesSearchBarWhenScrolling = false
 		sctrl.searchBar.delegate = self
 		definesPresentationContext = true
+		sctrl.searchResultsUpdater = self
 		return sctrl
 	}()
 
+	var searchCompleter = MKLocalSearchCompleter()
+	var searchResults = [MKLocalSearchCompletion]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+		title = "Add Location"
+		searchCompleter.delegate = self
     }
 
 	override func viewDidAppear(_ animated: Bool) {
@@ -32,25 +38,58 @@ class FindTableViewController: UITableViewController {
 		searchController.searchBar.becomeFirstResponder()
 	}
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-}
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return searchResults.count
+	}
 
-extension FindTableViewController: UISearchResultsUpdating {
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let searchResult = searchResults[indexPath.row]
+		let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+		cell.textLabel?.text = searchResult.title
+		cell.detailTextLabel?.text = searchResult.subtitle
+		return cell
+	}
 
-	func updateSearchResults(for searchController: UISearchController) {
-		let text = searchController.searchBar.text ?? ""
-		print(text)
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+		let completion = searchResults[indexPath.row]
+
+		let searchRequest = MKLocalSearch.Request(completion: completion)
+		let search = MKLocalSearch(request: searchRequest)
+		search.start { (response, _) in
+			guard let response = response, let item = response.mapItems.first else {
+				print("Missing coordinate")
+				return
+			}
+			let coordinate = item.placemark.coordinate
+			print(String(describing: coordinate))
+		}
 	}
 }
 
 extension FindTableViewController: UISearchBarDelegate {
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+		searchCompleter.queryFragment = searchText
+	}
+}
 
+extension FindTableViewController: MKLocalSearchCompleterDelegate {
+	func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+		searchResults = completer.results
+		tableView.reloadData()
+	}
+}
+
+extension FindTableViewController: UISearchResultsUpdating {
+	func updateSearchResults(for searchController: UISearchController) {
+		let text = searchController.searchBar.text ?? ""
+		if text.isEmpty {
+			searchResults.removeAll()
+			tableView.reloadData()
+		}
+	}
 }
