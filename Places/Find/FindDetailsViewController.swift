af86b2bbc31b30fa62d9e3e2747b6a06298de295
Michael <mik@use.startmail.com>
2019-04-10 22:04:48 +1000

    picker to pick category for new place

--- Places/Find/FindDetailsViewController.swift
+++ Places/Find/FindDetailsViewController.swift
@@ -15,6 +15,9 @@ class FindDetailsViewController: UIViewController, MKMapViewDelegate {
 	let coordinator: FindCoordinator
 	let item: MKMapItem
 	let categoriesController: CategoryController
+	let locationPicker = UIPickerView()
+	var pickerData = [(key: MarkerColor, value: String)]()
+	var selectedMarker: MarkerColor?
 
 	init(coordinator: FindCoordinator, item: MKMapItem, categoriesController: CategoryController) {
 		self.coordinator = coordinator
@@ -30,6 +33,14 @@ class FindDetailsViewController: UIViewController, MKMapViewDelegate {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setUpView()
+
+		// convert dict to tuple so can easily manage data
+		pickerData = categoriesController.categories.map { (key: MarkerColor, value: String) in
+			return (key, value)
+		}
+
+		pickerData.sort(by: { $0.value < $1.value })
+		categoryPickerSetUp()
 	}
 
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -45,6 +56,15 @@ class FindDetailsViewController: UIViewController, MKMapViewDelegate {
 		pin.coordinate = item.placemark.coordinate
 		detailsView.mapView.addAnnotation(pin)
 		detailsView.mapView.showAnnotations([pin], animated: true)
+		detailsView.nameTF.delegate = self
+		detailsView.categoryTF.delegate = self
+	}
+
+	func categoryPickerSetUp() {
+		locationPicker.showsSelectionIndicator = true
+		locationPicker.delegate = self
+		locationPicker.dataSource = self
+		detailsView.categoryTF.inputView = locationPicker
 	}
 
 	@objc func savePlace() {
@@ -53,7 +73,12 @@ class FindDetailsViewController: UIViewController, MKMapViewDelegate {
 			return
 		}
 
-		let newPlace = coordinator.savePlaceWith(mapItem: item, marker: .red, name: name)
+		guard let marker = selectedMarker else {
+			SCLAlertView(appearance: AlertService.standard).showError("Missing Marker")
+			return
+		}
+
+		let newPlace = coordinator.savePlaceWith(mapItem: item, marker: marker, name: name)
 		SCLAlertView(appearance: AlertService.standard).showSuccess("Saved")
 		view.endEditing(true)
 		detailsView.mapView.removeAnnotations(detailsView.mapView.annotations)
@@ -85,5 +110,37 @@ class FindDetailsViewController: UIViewController, MKMapViewDelegate {
 
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+// MARK: - UIPickerViewDelegate
+extension FindDetailsViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		return pickerData[row].value
+	}
+
+	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+		detailsView.categoryTF.text = pickerData[row].value
+		selectedMarker = pickerData[row].key
+	}
+
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		return 1
+	}
+
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return categoriesController.categories.count
+	}
+}
+
+extension FindDetailsViewController: UITextFieldDelegate {
+
+	// when TF selected and empty will default to first time in picker data
+	func textFieldDidBeginEditing(_ textField: UITextField) {
+		if textField == detailsView.categoryTF && textField.string.isEmpty {
+			detailsView.categoryTF.text = pickerData.first!.value
+			selectedMarker = pickerData.first!.key
+		}
 	}
 }
