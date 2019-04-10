6b3f49cd85b1812419f69955851300a7b9752b89
Michael <mik@use.startmail.com>
2019-04-11 08:34:46 +1000

    show place custom marker in details view

--- Places/Home/Details View/HomeDetailsViewController.swift
+++ Places/Home/Details View/HomeDetailsViewController.swift
@@ -34,6 +34,7 @@ class HomeDetailsViewController: UIViewController {
 		view.backgroundColor = .white
 		title = place.name
 		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deletePlace))
+		detailsView.mapView.delegate = self
 	}
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -54,5 +55,18 @@ class HomeDetailsViewController: UIViewController {
 
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+extension HomeDetailsViewController: MKMapViewDelegate {
+
+	// will show the custom map marker if is a place
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		guard let place = annotation as? Place else {
+			return nil // default marker will be used
+		}
+		let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+		annotationView.image = place.markerImage
+		return annotationView
 	}
 }
