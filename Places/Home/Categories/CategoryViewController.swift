ac888dd07450716aab53933a54f52d2a05abac03
Michael <mik@use.startmail.com>
2019-04-11 11:51:31 +1000

    all cells added

--- Places/Home/Categories/CategoryViewController.swift
+++ Places/Home/Categories/CategoryViewController.swift
@@ -13,11 +13,26 @@ class CategoryViewController: UITableViewController {
 	let coordinator: HomeCoordinator
 	let categoryCtrl: CategoryController
 
+	let cyanCell = UITableViewCell()
+	let greenCell = UITableViewCell()
+	let orangeCell = UITableViewCell()
 	let blueCell = UITableViewCell()
+	let purpleCell = UITableViewCell()
 	let redCell = UITableViewCell()
+	let whiteCell = UITableViewCell()
 
+	var cyanCellText: UITextField!
+	var greenCellText: UITextField!
+	var orangeCellText: UITextField!
 	var blueCellText: UITextField!
+	var purpleCellText: UITextField!
 	var redCellText: UITextField!
+	var whiteCellText: UITextField!
+
+	private var cells = [UITableViewCell]()
+	private var textFields = [UITextField]()
+	private var labels = [String]()
+	private var markers = [MarkerColor]()
 
 	init(categoryCtrl: CategoryController, coordinator: HomeCoordinator) {
 		self.categoryCtrl = categoryCtrl
@@ -28,29 +43,49 @@ class CategoryViewController: UITableViewController {
 	override func loadView() {
 		super.loadView()
 		// construct first name cell, section 0, row 0
-		blueCellText = UITextField(frame: CGRect(x: 20, y: 0, width: blueCell.frame.width, height: blueCell.frame.height))
-		blueCell.addSubview(blueCellText)
+		blueCellText = setUp(cell: blueCell)
+		redCellText = setUp(cell: redCell)
+		cyanCellText = setUp(cell: cyanCell)
+		greenCellText = setUp(cell: greenCell)
+		orangeCellText = setUp(cell: orangeCell)
+		purpleCellText = setUp(cell: purpleCell)
+		whiteCellText = setUp(cell: whiteCell)
 
-		redCellText = UITextField(frame: CGRect(x: 20, y: 0, width: redCell.frame.width, height: redCell.frame.height))
-		redCell.addSubview(redCellText)
+		// set up data arrays
+		cells = [cyanCell, greenCell, orangeCell, blueCell, purpleCell, redCell, whiteCell]
+		textFields = [cyanCellText, greenCellText, orangeCellText, blueCellText, purpleCellText, redCellText, whiteCellText]
+		labels = ["Cyan", "Green", "Orange", "Blue", "Purple", "Red", "White"]
+		markers = [.cyan, .green, .orange, .blue, .purple, .red, .white]
+		assert(cells.count == labels.count && labels.count == markers.count && labels.count == textFields.count)
+	}
 
-		[redCellText, blueCellText].forEach { tf in
-			tf.placeholder = "Custom Category Name"
-		}
+	private func setUp(cell: UITableViewCell) -> UITextField {
+		let tf = UITextField(frame: CGRect(x: 20, y: 0, width: cell.frame.width, height: cell.frame.height))
+		tf.placeholder = "Custom Category Name"
+		cell.addSubview(tf)
+		return tf
 	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		tableView = UITableView(frame: view.frame, style: .grouped)
-		view.backgroundColor = .groupTableViewBackground
 		title = "Categories"
+		setUpTableView()
+		fillCells()
+	}
+
+	private func setUpTableView() {
+		tableView = UITableView(frame: view.frame, style: .grouped)
 		tableView.delegate = self
 		tableView.dataSource = self
 		tableView.allowsSelection = false
+	}
 
-		blueCellText.text = categoryCtrl.categories[.blue]
-		redCellText.text = categoryCtrl.categories[.red]
-
+	private func fillCells() {
+		for index in 0..<markers.count {
+			let currentMarker = markers[index]
+			let currentTextField = textFields[index]
+			currentTextField.text = categoryCtrl.categories[currentMarker]
+		}
 	}
 
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -58,40 +93,19 @@ class CategoryViewController: UITableViewController {
 	}
 
 	override func numberOfSections(in tableView: UITableView) -> Int {
-		return 2
+		return cells.count
 	}
 
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		switch section {
-		case 0: return 1    // section 0 has 1 rows
-		case 1: return 1    // section 1 has 1 row
-		default: fatalError("Unknown number of sections")
-		}
+		return 1
 	}
 
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-		switch indexPath.section {
-		case 0:
-			switch indexPath.row {
-			case 0: return blueCell   // section 0, row 0 is the blue
-			default: fatalError("Unknown row in section 0")
-			}
-		case 1:
-			switch indexPath.row {
-			case 0: return redCell      // section 1, row 0 is red
-			default: fatalError("Unknown row in section 1")
-			}
-		default: fatalError("Unknown section")
-		}
+		return cells[indexPath.section] // only one cell per section so can do this
 	}
 
 	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-		switch section {
-		case 0: return "Blue Icon"
-		case 1: return "Red Icon"
-		default: fatalError("Unknown section")
-		}
+		return labels[section]
 	}
 
 	required init?(coder aDecoder: NSCoder) {
@@ -99,11 +113,14 @@ class CategoryViewController: UITableViewController {
 	}
 
 	override func viewWillDisappear(_ animated: Bool) {
-		save(textField: blueCellText, marker: .blue)
-		save(textField: redCellText, marker: .red)
+		// save as necessary
+		for index in 0..<markers.count {
+			save(textField: textFields[index], marker: markers[index])
+		}
 	}
 
 	func save(textField: UITextField, marker: MarkerColor) {
+		// only save if name has been changed and is not empty string
 		let isValidString = !textField.string.isEmpty
 		let nameHasChanged = textField.string != categoryCtrl.categories[marker]
 		if isValidString && nameHasChanged {
@@ -111,5 +128,4 @@ class CategoryViewController: UITableViewController {
 			categoryCtrl.categories[marker] = textField.string
 		}
 	}
-
 }
